27202075fa7ee044d61a22b2571eebf03cdc2fb8
maryjean <maryjanecugal@gmail.com>
2017-12-19 16:04:32 +0800

    3rd wave of labs

---
+++ 10-create-follows-table.sql
@@ -0,0 +1,15 @@
+-- Create a table named "follows" with the following columns:
+--     id           (integer)       primary key, auto increment
+--     following    (string)        the email of the person being followed,
+--                                  should be a foreign key to the "users" table
+--     follower     (string)        the email of the person that follows,
+--                                  should be a foreign key to the "users" table
+--     created_at   (timestamp)     default to current timestamp
+--
+-- Write your query below:
+CREATE TABLE follows IF NOT EXISTS(
+	id SERIAL PRIMARY KEY,
+	following VARCHAR(15)  REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE NOT NULL,
+	follower VARCHAR(15) REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE NOT NULL,
+	created_at timestamp DEFAULT now()
+);
