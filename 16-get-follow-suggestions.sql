d14eb397dd70d67c03aa930a97f31913c0c4b4b3
maryjean <maryjanecugal@gmail.com>
2017-12-19 18:00:05 +0800

    Delete 16-get-follow-suggestions.sql

--- 16-get-follow-suggestions.sql
+++
@@ -1,8 +0,0 @@
--- Get follow suggestions for a given user. Suggestions should be those other
--- users that the given user does not follow yet.
---
--- You have access to the following variables which can be used as placeholders
--- for actual values:
---     - email
---
--- Write your query below:
