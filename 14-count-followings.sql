a2a70dd75cd9f141a3d5f8f55552808328d4c3e1
maryjean <maryjanecugal@gmail.com>
2017-12-19 17:59:27 +0800

    Delete 14-count-followings.sql

--- 14-count-followings.sql
+++
@@ -1,7 +0,0 @@
--- Count how many users the given user is following.
---
--- You have access to the following variables which can be used as placeholders
--- for actual values:
---     - email
---
--- Write your query below:
